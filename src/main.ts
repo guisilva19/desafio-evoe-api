bb0735b5621b694771eb940c759e30e1258f0201
unknown <2003silvagui@gmail.com>
2025-03-21 16:00:52 -0300

    feat: README.md

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT || 3000);
   console.log(
-    `🚀 Server running at: http://localhost:${process.env.PORT || 3000}/docs`,
+    `🚀 Server running at: http://localhost:${process.env.PORT || 8000}/docs`,
   );
 }
 bootstrap();
